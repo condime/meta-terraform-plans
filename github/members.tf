f7a4461b1671733b2dc12256682fa0973334764f
Ben Cordero <bencord0@condi.me>
2021-10-07 13:17:03 +0100

    Import github teams

--- github/members.tf
+++ github/members.tf
@@ -1,4 +1,20 @@
-resource "github_membership" "bencord0" {
-  username = "bencord0"
+locals {
+  admins = ["bencord0"]
+
+  members = [
+    "catzkorn",
+  ]
+}
+
+
+resource "github_membership" "admins" {
+  for_each = toset(local.admins)
+  username = each.key
   role     = "admin"
+}
+
+resource "github_membership" "members" {
+  for_each = toset(local.members)
+  username = each.key
+  role     = "member"
 }
--- github/teams.tf
+++ github/teams.tf
@@ -3,13 +3,21 @@ resource "github_team" "admins" {
   privacy = "closed"
 }
 
-resource "github_team_membership" "admins_bencord0" {
-  team_id  = "${github_team.admins.id}"
-  username = "bencord0"
-  role     = "maintainer"
+resource "github_team_membership" "admins" {
+  for_each = toset(local.admins)
+  team_id  = github_team.admins.id
+  username = each.key
+  role     = "member"
 }
 
 resource "github_team" "notadmin" {
   name    = "notadmin"
   privacy = "closed"
+}
+
+resource "github_team_membership" "notadmin" {
+  for_each = toset(concat(local.admins, local.members))
+  team_id  = github_team.notadmin.id
+  username = each.key
+  role     = "member"
 }
