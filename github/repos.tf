81121f1035d0d195326262ccbcb99cd0212d48ad
Ben Cordero <bencord0@condi.me>
2021-10-19 20:30:29 +0100

    Add condime/zones repository

--- github/repos.tf
+++ github/repos.tf
@@ -39,5 +39,38 @@ resource "github_repository" "secrets" {
   allow_rebase_merge = true
   allow_squash_merge = true
 
+  visibility           = "private"
   vulnerability_alerts = true
+}
+
+# Code of Conduct. How we interact with each other
+resource "github_repository" "code-of-conduct" {
+  name = "code-of-conduct"
+  description = "Code of Conduct for the condi.me Slack Group"
+
+  has_downloads = false
+  has_issues    = false
+  has_wiki      = false
+
+  allow_merge_commit = true
+  allow_rebase_merge = true
+  allow_squash_merge = true
+
+  vulnerability_alerts = false
+}
+
+# Example DNS Zone files, taken for the condi.me domain
+resource "github_repository" "zones" {
+  name = "zones"
+
+  has_downloads = false
+  has_issues    = false
+  has_wiki      = false
+
+  allow_merge_commit = false
+  allow_rebase_merge = true
+  allow_squash_merge = true
+
+  visibility           = "private"
+  vulnerability_alerts = false
 }
