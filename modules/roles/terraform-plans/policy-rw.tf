0a3cca40315218bc921025ef39b62d247de7d105
Ben Cordero <bencord0@condi.me>
2021-12-28 20:02:15 +0000

    Grant terraform-plans access to `cognito-idp`

--- modules/roles/terraform-plans/policy-rw.tf
+++ modules/roles/terraform-plans/policy-rw.tf
@@ -18,4 +18,52 @@ data "aws_iam_policy_document" "terraform-plans" {
       "*",
     ]
   }
+
+  statement {
+    actions = [
+      "cognito-idp:ListIdentityProviders",
+      "cognito-idp:ListTagsForResource",
+      "cognito-idp:ListUserPools",
+      "cognito-idp:ListUserPoolClients",
+      "cognito-idp:ListUsers",
+      "cognito-idp:ListGroups",
+      "cognito-idp:ListUsersInGroup",
+
+      "cognito-idp:DescribeIdentityProvider",
+      "cognito-idp:DescribeUserPool",
+      "cognito-idp:DescribeUserPoolClient",
+      "cognito-idp:DescribeUserPoolDomain",
+      "cognito-idp:GetUser",
+      "cognito-idp:GetGroup",
+
+      "cognito-idp:GetIdentityProviderByIdentifier",
+      "cognito-idp:GetPoolMfaConfig",
+
+      "cognito-idp:TagResource",
+      "cognito-idp:UntagResource",
+
+      "cognito-idp:CreateUserPool",
+      "cognito-idp:CreateUserPoolClient",
+      "cognito-idp:CreateUserPoolDomain",
+      "cognito-idp:CreateIdentityProvider",
+      "cognito-idp:CreateGroup",
+
+      "cognito-idp:DeleteUserPool",
+      "cognito-idp:DeleteUserPoolClient",
+      "cognito-idp:DeleteUserPoolDomain",
+      "cognito-idp:DeleteIdentityProvider",
+      "cognito-idp:DeleteGroup",
+      "cognito-idp:DeleteUser",
+
+      "cognito-idp:UpdateGroup",
+      "cognito-idp:UpdateIdentityProvider",
+      "cognito-idp:UpdateUserPool",
+      "cognito-idp:UpdateUserPoolClient",
+      "cognito-idp:UpdateUserPoolDomain",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
 }
