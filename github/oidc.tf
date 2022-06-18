838e0c7eac7093eeb1326e042b3d7b9208e80098
Ben Cordero <bencord0@condi.me>
2022-06-18 21:53:32 +0100

    Update github actions oidc token fingerprint

--- github/oidc.tf
+++ github/oidc.tf
@@ -4,7 +4,11 @@ resource "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
 
   # Issued by DigiCert Inc, Valid from 10/22/2013 until 10/22/2028
-  thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]
+  # $ openssl s_client -servername token.actions.githubusercontent.com \
+  #     -showcerts -connect token.actions.githubusercontent.com:443 \
+  #     | openssl x509 -fingerprint -noout \
+  #     | cut -d'=' -f 2 | sed -e 's/://g'
+  thumbprint_list = ["15E29108718111E59B3DAD31954647E3C344A231"]
 
   # GitHub Org
   client_id_list = [
